407f00fe702ddc161242f9659cc924d2deca1684
Lupita Reyes <lupita.g.reyes@outlook.com>
2023-03-02 01:51:18 -0500

    Implement function to render hardcoded tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,51 +6,72 @@
 
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
-const createTweetElement = (tweet) => {
-  const tweetUser = tweetData.user;
 
-  const $tweet = `
+const createTweetElement = (data) => {
+  const { user, content, created_at } = data;
+
+  let $tweet = `
+  <section class="tweet">
   <header>
           <div>
             <img
-              src=${tweetUser.avatars}
+              src=${user.avatars}
               alt="logo"
               width="50"
               height="50"
             />
-            <h3>${tweetUser.name}</h3>
+            <h3>${user.name}</h3>
           </div>
-          <h3>${tweetUser.handle}</h3>
+          <h3>${user.handle}</h3>
         </header>
-        <article>${tweetData.content.text}</article>
+        <article>${content.text}</article>
         <footer>
-          <p>${tweetData.created_at}</p>
+          <p>${created_at}</p>
           <div>
             <i class="fa-solid fa-flag"></i>
             <i class="fa-solid fa-retweet"></i>
             <i class="fa-solid fa-heart"></i>
           </div>
         </footer>
+        </section>
   `
   return $tweet;
 }
 
+const renderTweets = (tweets) => {
+  for (const tweet of tweets) {
+    const newTweet = createTweetElement(tweet);
+    $("#tweet-container").append(newTweet);
+  }
+}
+
 
 $(document).ready(function() {
-  const $tweet = createTweetElement(tweetData);
-  console.log($tweet)
-  $(".tweet").append($tweet);
-
+  renderTweets(data);
 });
